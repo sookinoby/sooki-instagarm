efe89660814ce8d45a4b16e3a808e8f9379b4cd5
Alex Aravind <alexaravind@Alexs-MacBook-Pro-3.local>
2015-07-13 23:50:09 +0530

    changed config

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
- db : 'localhost',
+ db :'mongodb://sooki:test123@ds029847.mongolab.com:29847/newdb',
  clientSecret: process.env.clientSecret || '26d03e3ee1894ae39ae0f67ceb63c928',
  tokenSecret: process.env.tokenSecret || 'sooki'
 }
